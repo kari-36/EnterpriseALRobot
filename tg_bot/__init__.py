d521d24dc2acdc610e7d407c7a0d709d1bf2a42a
Hell God <83143204+kari-36@users.noreply.github.com>
2021-09-20 18:04:23 +0500

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -129,7 +129,7 @@ else:
 
 from tg_bot.modules.sql import SESSION
 
-updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10}, persistence=PostgresPersistence(SESSION))
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient(MemorySession(), APP_ID, API_HASH)
 dispatcher = updater.dispatcher
 
