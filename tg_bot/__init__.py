ac1f9737c0768eb03d9c1b0357cf1554de7ec03c
Hell God <83143204+kari-36@users.noreply.github.com>
2021-09-20 19:01:42 +0500

    Noni

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -136,10 +136,8 @@ dispatcher = updater.dispatcher
 
 SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
-WHITELIST_USERS = list(WHITELIST_USERS)
-SARDEGNA_USERS = list(SARDEGNA_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
-
+SARDEGNA_USERS = list(SARDEGNA_USERS)
 
 
 kp = Client(":memory:", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN, workers=min(32, os.cpu_count() + 4))
